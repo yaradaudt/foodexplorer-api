e114cc9cba7bbc365451c1b798ed38240440fb5b
Yara Daudt <yaracrisalis@gmail.com>
2024-06-17 22:31:22 +0100

    Updating AppError and HTTP codes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError =  require("../utils/AppError")
+
 class UsersController { // 5 methods to each controller => index (GET), show (GET), Create (POST), Update (PUT), delete (DELETE).
     create(request, response) {
         const { name, email, password } = request.body
+
+        if(!name) {
+            throw new AppError("Nome é obrigatório!")
+        }
 
         response.status(201).json({ name, email, password })
     }
