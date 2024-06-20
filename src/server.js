b07175500b299e334fbfd7e7e31c96dc292385b5
Yara Daudt <yaracrisalis@gmail.com>
2024-06-20 17:49:38 +0100

    Sqlite database connected

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+require("express-async-errors")
+const database = require("./database/sqlite")
 const AppError = require("./utils/AppError")
+
 const express = require("express")
 
 const routes = require("./routes")
@@ -7,6 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
