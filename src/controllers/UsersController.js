ab8932b3badb06270771311365b39bf789b0fd3c
Yara Daudt <yaracrisalis@gmail.com>
2024-06-24 18:25:46 +0100

    Admin permissions included

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -10,7 +10,7 @@ function validatePasswordLength(password) {
 
 class UsersController { // 5 methods to each controller => index (GET), show (GET), Create (POST), Update (PUT), delete (DELETE).
     async create(request, response) {
-        const { name, email, password } = request.body
+        const { name, email, password, is_admin = false } = request.body
 
         const database = await sqliteConnection()
         const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
@@ -24,15 +24,15 @@ class UsersController { // 5 methods to each controller => index (GET), show (GE
         const hashedPassword = await hash(password, 8)
 
         await database.run(
-            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, hashedPassword]
+            "INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)",
+            [name, email, hashedPassword, is_admin]
         )
 
         return response.status(201).json()
     }
 
     async update(request, response) {
-        const { name, email, password, old_password } = request.body
+        const { name, email, password, old_password, is_admin } = request.body
         const { id } = request.params
 
         const database = await sqliteConnection()
@@ -48,34 +48,44 @@ class UsersController { // 5 methods to each controller => index (GET), show (GE
             throw new AppError("Este e-mail já está em uso.")
         }
 
-        user.name = name
-        user.email = email
+        user.name = name ?? user.name
+        user.email = email ?? user.email
 
-        if(password && !old_password) {
-            throw new AppError("Você precisa informar a senha antiga para definir uma nova senha.")
-        }
+        if(password) {
+            validatePasswordLength(password)
 
-        if(password && old_password) {
+            if(!old_password) {
+                throw new AppError("Você precisa informar a senha antiga para definir uma nova senha.")
+            }
+
             const checkOldPassword = await compare(old_password, user.password)
-
-            if(!checkOldPassword) {
+        
+            if (!checkOldPassword) {
                 throw new AppError("A senha antiga não confere.")
             }
-            
-            user.password = await hash(password,8)
-            
-        }
-        
-        validatePasswordLength(password)
 
+            user.password = await hash(password, 8)
+        }
+
+        if (is_admin !== undefined) {
+            if (!user.is_admin && user.id !== request.user.id) {
+                throw new AppError("Você não tem permissão para atualizar o campo 'is_admin'.", 403)
+            }
+
+            user.is_admin = is_admin
+        } else {
+            user.is_admin = user.is_admin
+        }  
+        
         await database.run(`
         UPDATE users SET
         name = ?,
         email = ?, 
         password = ?,
-        updated_at = ?
+        is_admin = ?,
+        updated_at = DATETIME('now')
         WHERE id = ?`, 
-        [user.name, user.email, user.password, new Date(), id])
+        [user.name, user.email, user.password, user.is_admin, id])
 
         return response.json()
     }
