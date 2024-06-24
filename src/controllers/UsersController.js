2971aa2f25530dbcfa9b9694208bb47951b47634
Yara Daudt <yaracrisalis@gmail.com>
2024-06-24 16:55:17 +0100

    Updating user password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError =  require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 
@@ -9,8 +9,12 @@ class UsersController { // 5 methods to each controller => index (GET), show (GE
         const database = await sqliteConnection()
         const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-        if (checkUserExists){
+        if(checkUserExists){
             throw new AppError("Este e-mail já está em uso.")
+        }
+
+        if(password.length < 6) {
+            throw new AppError("A senha precisa ter no mínimo 6 caracteres.")
         }
 
         const hashedPassword = await hash(password, 8)
@@ -24,13 +28,13 @@ class UsersController { // 5 methods to each controller => index (GET), show (GE
     }
 
     async update(request, response) {
-        const { name, email } = request.body
+        const { name, email, password, old_password } = request.body
         const { id } = request.params
 
         const database = await sqliteConnection()
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
 
-        if (!user) {
+        if(!user) {
             throw new AppError("Usuário não encontrado.")
         }
 
@@ -43,13 +47,34 @@ class UsersController { // 5 methods to each controller => index (GET), show (GE
         user.name = name
         user.email = email
 
+        if(password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir uma nova senha.")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+
+            if(!checkOldPassword) {
+                throw new AppError("A senha antiga não confere.")
+            }
+            
+            user.password = await hash(password,8)
+            
+        }
+
+        if(password.length < 6) {
+            throw new AppError("A senha precisa ter no mínimo 6 caracteres.")
+        }
+
+
         await database.run(`
         UPDATE users SET
         name = ?,
         email = ?, 
+        password = ?,
         updated_at = ?
         WHERE id = ?`, 
-        [user.name, user.email, new Date(), id])
+        [user.name, user.email, user.password, new Date(), id])
 
         return response.json()
     }
