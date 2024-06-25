ff24bb81a9bb2b2c34577adff6170ba0533cd0d8
Yara Daudt <yaracrisalis@gmail.com>
2024-06-25 19:39:03 +0100

    Created favorites table

--- src/database/knex/migrations/20240625183538_createCartItems.js
+++ src/database/knex/migrations/20240625183538_createCartItems.js
@@ -1,15 +1,9 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("cart_items", table => {
+    table.increments("id");
+    table.integer("cart_id").references("id").inTable("cart").onDelete("CASCADE");
+    table.integer("dish_id").references("id").inTable("dishes").onDelete("CASCADE");
+    table.integer("quantity").notNullable();
+    table.decimal("price", 10, 2).notNullable();
+});
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTable("cart_items");
