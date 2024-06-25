e047cd10bfe8faf2add99469b7d1eb6004d291c1
Yara Daudt <yaracrisalis@gmail.com>
2024-06-25 12:18:23 +0100

    Created Orders Migration

---
+++ src/database/knex/migrations/20240625104928_createOrders.js
@@ -0,0 +1,13 @@
+
+exports.up = knex => knex.schema.createTable("orders", table => {
+    table.increments("id")
+    table.text("status").notNullable();
+    table.decimal("price", 10, 2).notNullable();
+    table.text("payment_method").notNullable();
+
+  
+    table.integer("user_id").references("id").inTable("users");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+})
+
+exports.down = knex => knex.schema.dropTable("orders")
