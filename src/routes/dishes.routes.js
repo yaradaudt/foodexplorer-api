c4f2c6686f50843aa4532b975dddf37ed1a0264e
Yara Daudt <yaracrisalis@gmail.com>
2024-06-27 16:40:15 +0100

    Created show method

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -6,5 +6,7 @@ const dishesRoutes = Router()
 const dishesController = new DishesController()
 
 dishesRoutes.post("/:user_id", dishesController.create)
+dishesRoutes.get("/:id", dishesController.show)
+
 
 module.exports = dishesRoutes
