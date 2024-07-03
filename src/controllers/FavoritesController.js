fb52f783c1c54a3453bbe19e34c312d7cff30aeb
Yara Daudt <yaracrisalis@gmail.com>
2024-07-03 11:11:05 +0100

    Listing favorites

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -5,13 +5,35 @@ class FavoritesController {
         const { dish_id } = request.body
         const { user_id } = request.params
 
-        const favorites = await knex("favorites").insert({
+        const favorite = await knex("favorites").insert({
         dish_id,
         user_id
         })
 
-        return response.status(201).json(favorites)
+        return response.status(201).json(favorite)
+    }
+
+    async index(request, response) {
+        try {
+            const { user_id } = request.query
+
+            const favorites = await knex("favorites")
+                .select([
+                    "dishes.id as dish_id",
+                    "dishes.title",
+                    "dishes.description",
+                    "dishes.category",
+                    "dishes.price"
+                ])
+                .where("favorites.user_id", user_id)
+                .innerJoin("dishes", "dishes.id", "favorites.dish_id")
+            
+                return response.status(200).json(favorites);
+            } catch (error) {
+              console.error(error);
+              return response.status(500).json({ message: "Erro ao listar os favoritos." });
+            }
     }
 }
-
+        
 module.exports = FavoritesController
--- src/routes/favorites.routes.js
+++ src/routes/favorites.routes.js
@@ -5,6 +5,7 @@ const FavoritesController = require("../controllers/FavoritesController")
 const favoritesRoutes = Router()
 const favoritesController = new FavoritesController()
 
+favoritesRoutes.get("/", favoritesController.index)
 favoritesRoutes.post("/:user_id", favoritesController.create)
 
 module.exports = favoritesRoutes
