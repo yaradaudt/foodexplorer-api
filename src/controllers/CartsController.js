c53147875eb6dcaf7749068eb3483c16aaf042d1
Yara Daudt <yaracrisalis@gmail.com>
2024-07-12 13:04:50 +0100

    Updating cart

--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -1,6 +1,6 @@
 const knex = require("../database/knex")
 
-class CartsController {
+class CartsController { // regular user only
     async create(request, response) {
         const { dish_id, quantity } = request.body;
         const { user_id } = request.params;
@@ -25,6 +25,32 @@ class CartsController {
             });
 
             response.status(201).json({ message: "Item adicionado ao carrinho" });
+    }
+
+    async update(request, response) {
+        const { user_id, dish_id } = request.params
+        const { quantity } = request.body
+
+        const cart = await knex("cart").where({ user_id }).first()
+
+        if (!cart) {
+            return response.status(404).json({ message: "Carrinho não encontrado" })
+        }
+
+        const dish = await knex("dishes").where({ id: dish_id }).first();
+            
+        if (!dish) {
+            return response.status(404).json({ message: "Prato não encontrado" })
+        }
+
+        await knex("cart_items")
+            .where({ cart_id: cart.id, dish_id })
+            .update({
+                quantity,
+                price: dish.price * quantity
+            });
+
+        response.status(200).json()
     }
 }
 
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -7,5 +7,7 @@ const cartsRoutes = Router()
 const cartsController = new CartsController()
 
 cartsRoutes.post("/:user_id", cartsController.create)
+cartsRoutes.put("/:user_id/:dish_id", cartsController.update)
+
 
 module.exports = cartsRoutes
