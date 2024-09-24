76dc7aaeb22ff07bcd1ef94018dda292986a4ad5
Yara Daudt <yaracrisalis@gmail.com>
2024-09-24 16:50:36 +0100

    Cors and axios

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ require("express-async-errors")
 const AppError = require("./utils/AppError")
 const uploadConfig = require("./configs/upload")
 
+const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
